a670e43f5e0c2d1ab759d61e055680a397f660fe
Martin Hruska <hruskamartin25@gmail.com>
2015-02-12 17:46:06 +0100

    added examples of statements to micro instruction transformation

--- src/micro.c
+++ src/micro.c
@@ -2,16 +2,24 @@
 
 struct T {
 	struct T* next;
+	int data;
 };
 
 
 int main()
 {
-	struct T* x;
+	struct T* x, *y;
 	
 	x = (struct T*) malloc(sizeof(struct T));
+	x->next = (struct T*) malloc(sizeof(struct T));
+
+	y = x->next;
+	x->data = 0;
+	y->data = 0;
+	x->data = y->data;
 
 	free(x);
+	free(y);
 
 	return 1;
 }
